19bf8219c2315ed71cfe3752b5544e5dbdbd2d29
Andrea Lacson <andrealacson@Andreas-MacBook-Pro.local>
2019-04-15 16:00:54 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,20 +2,61 @@ var elModal = document.getElementById('modal');
 var elSignInBtn = document.getElementById('signin');
 var elCloseBtn = document.getElementById('close');
 var elSubBtn = document.getElementById('submit');
-var elInput = document.getElementsByTagName('input');
+// var elInput = document.querySelectorAll('.field');
+// var elInput = document.querySelector('.field');
+var elInputUser = document.getElementById('user');
+var elInputPass = document.getElementById('pass');
+// var elInput = document.getElementsByTagName('input');
 
 function openModal() {
 elModal.style.display = 'block';
 }
 
 elSignInBtn.addEventListener('click', function(e) {
-  elModal.classList.toggle('open');
+  elModal.classList.add('open');
+  elModal.classList.remove('close');
 });
 
 elCloseBtn.addEventListener('click', function(e){
-  elModal.classList.toggle('close');
+  elModal.classList.add('close');
+  elModal.classList.remove('open');
+  elInputUser.classList.remove('error');
+  elInputPass.classList.remove('error');
 });
 
 elSubBtn.addEventListener('click', function(e){
-  elInput.classlist.toggle('error');
+  elInputUser.classList.add('error');
+  elInputPass.classList.add('error');
+
+  // if the value in either field is blank: '', add the error
+  // trim the input
+  // If the vale in either field is less than x character (length), add the error
+
+  // '' == ''
+  // ' test '.trim
+  // ''.trim() == ''
+  // "testing".length > 0
 });
+
+elInputUser.addEventListener('focus', function(e){
+  this.classList.remove('error');
+});
+
+// elInputUser.addEventListener('mouseout', function(e){
+//   this.classList.add('error');
+// });
+
+elInputPass.addEventListener('focus', function(e){
+  this.classList.remove('error');
+
+// var elInput = document.querySelectorAll('.field');
+// allInputFields.forEach (field => {
+//   field.
+// })
+
+
+});
+//
+// elInputPass.addEventListener('mouseout', function(e){
+//   this.classList.add('error');
+// });
